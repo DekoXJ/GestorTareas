3591444be90f7e51f5cb30dd1d4fad7dbe3f8ad5
71586559 <71586559@continental.edu.pe>
2024-06-04 00:01:50 -0500

    Listo lanzamiento 1.0.1

--- src/logica/gestor_tareas.py
+++ src/logica/gestor_tareas.py
@@ -14,3 +14,18 @@ class GestorTareas:
             raise ValueError("El título no puede estar vacío")
         tarea = Tarea(titulo, descripcion)
         self.tareas.append(tarea)
+
+    def obtener_tareas(self):
+        return self.tareas
+
+    def marcar_completada(self, indice):
+        if 0 <= indice < len(self.tareas):
+            self.tareas[indice].completada = True
+        else:
+            raise IndexError("Índice fuera de rango")
+
+    def eliminar_tarea(self, indice):
+        if 0 <= indice < len(self.tareas):
+            del self.tareas[indice]
+        else:
+            raise IndexError("Índice fuera de rango")
